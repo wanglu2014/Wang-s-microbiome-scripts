837193ad282fd67a77442c083afc9c8d061ed851
wanglu2014 <wanglu2014@sibcb.ac.cn>
2019-01-18 15:29:40 +0800

    Add files via upload

--- plot_significant.R
+++ plot_significant.R
@@ -5,6 +5,6 @@ attr_meta<- read.csv("~/netattrmeta.csv")
 compared_list<-list(c("Chongming Nonsmoker","Shanghai Smoker"))
 ggplot(data = attr_meta, mapping = aes(x=Subgroup,y =mean.degree)) +
   geom_violin(alpha = 0) +
-  stat_summary(fun.y=mean, colour="darkred", geom="point", hape=18, size=3,show_guide = FALSE)+
+  stat_summary(fun.y=mean, colour="darkred", geom="point", shape=18, size=3,show.legend = F)+
   geom_jitter(alpha = 0.8, color = "tomato")+
   geom_signif(comparisons = compared_list,test=t.test,map_signif_level = TRUE, textsize=3, step_increase = 0.2)
